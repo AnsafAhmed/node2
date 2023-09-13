e2fd30df9cae904bab9134d30ffbffc4de938121
Ansaf Ahmed <ansaf787@gmail.com>
2023-09-13 06:34:29 +0000

    handle /

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.send("Node2");
+})
+
 app.listen(PORT, () => {
     console.log("Server listening on port ", PORT);
 })
